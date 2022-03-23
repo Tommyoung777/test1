3c9c3de0db3be2549bc4151e01c2acedb561cb4c
Tommyoung777 <tommyoung2002@gmail.com>
2022-03-23 22:06:39 +1300

    initial commit

---
+++ 初学.py
@@ -0,0 +1,112 @@
+# def divide(a,b,c):
+#     d = (a / b)
+#
+#
+#
+#      print(format(d), '.cd')
+
+# d = input('divide')
+#
+# for i in range(len(d)):
+#     if d[i] == '.':
+#         index_space = i
+
+# def divide(volume):
+#     for i in range(len(volume)):
+#         if volume[i] == ',':
+#
+#             index_space = i
+#
+#             a = volume[:index_space]
+#             b = volume[index_space:index_space]
+#             c = volume[::index_space]
+#
+#             d = (a / b)
+#             print(format(d), '.cd')
+#
+#
+
+
+# def main()
+#     the_volume = get_volume()
+#     initials = get_initials(the_volume)
+#
+#
+# def get_volume()
+#     volume = input('count decimal places')
+#     return volume
+#
+#
+# def get_initials(the_volume)
+#     dot = the_volume.find('.')
+#     return the_volume[0] + the_volume[dot + 1]
+#
+#
+# main()
+#
+#
+
+# class ATM(object):
+#     def __init__(self,my_money):
+#         self.money = my_money
+#
+#     def save_money(self,add_money):
+#         self.money += add_money
+#
+#     def get_money(self,money_get):
+#         self.money -=money_get
+#
+#     def check_money(self):
+#         return self.money
+#
+# if __name__ == '__main__':
+#     ATM_a = ATM(my_money=1000)
+#     ATM_a.save_money(100)
+#     print(ATM_a.check_money())
+#
+#
+#
+#     ATM_b = ATM(my_money=3000)
+#     ATM_b.get_money(500)
+#     print(ATM_b.check_money())
+
+
+# def get_user_input():
+#     prompt = "Enter a number from 1 to 20: "
+#     user_input = input(prompt)
+#     return user_input
+#
+# import random
+# def generate_secret():
+#     secret = random.randrange(-19, 19, 1)
+#     return secret
+#
+#
+# def display_result(user_input, secret):
+#     result = (user_input - secret)
+#     result = abs(result)
+#     return result
+#
+#
+# def main():
+#     input_number = get_user_input()
+#     secret = generate_secret()
+#     result = display_result(int(input_number), secret)
+#     print('You entered', input_number, '.', 'The secret was', secret, '.', 'The difference is', result)
+#
+#
+# main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
